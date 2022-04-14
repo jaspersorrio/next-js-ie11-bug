2779692b0b87126ee9df9b70bdd08025e30e56c7
Jasper Yap <jasper@eezee.com.sg>
2022-04-14 18:02:43 +0800

    feat: added TestComponent

---
+++ components/test-component.tsx
@@ -0,0 +1,9 @@
+const TestComponent = () => {
+    return (
+        <span style={{ color: "red", fontSize: 100 }}>
+            This is a test component
+        </span>
+    );
+}
+
+export default TestComponent;
